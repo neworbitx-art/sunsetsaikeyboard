91f6174b9a467b20df5a503e1a73c32b78e787b2
Cristian Ramirez <neworbitx@gmail.com>
2026-06-27 02:24:52 -0600

    chore: create initial Sunsets Properties Xcode project

---
+++ SunsetsProperties/SunsetsProperties/SunsetsPropertiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunsetsPropertiesApp.swift
+//  SunsetsProperties
+//
+//  Created by Cristian Alonso on 27/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SunsetsPropertiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SunsetsProperties/SunsetsPropertiesTests/SunsetsPropertiesTests.swift
@@ -0,0 +1,19 @@
+//
+//  SunsetsPropertiesTests.swift
+//  SunsetsPropertiesTests
+//
+//  Created by Cristian Alonso on 27/06/26.
+//
+
+import Testing
+@testable import SunsetsProperties
+
+struct SunsetsPropertiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
